906c699c40fbfcd9bb7ab6d38841dd026995a103
nivaasgd <nivaas.damotharan@gmail.com>
2022-12-25 02:10:12 +0530

    updated port

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from userapp import app
+import os
 
 
 # start the flask app with MONGO DB config
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT'))
+    app.run(host='0.0.0.0', port=port)
